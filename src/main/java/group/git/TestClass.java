50df14d25bf68eccb1a98e03b20ab6bd470e87e9
star1109 <riyatalati1109@gmail.com>
2022-06-01 10:47:04 -1100

    riya branch

--- src/main/java/group/git/TestClass.java
+++ src/main/java/group/git/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("by Nirali");
         System.out.println("by riya");
+        System.out.println("by riya2");
     }
 }
