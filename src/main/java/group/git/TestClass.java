c112075aba59127d1623207666e8159c6d09f38b
Ninnirali <connectwithnirali@gmail.com>
2022-06-01 10:04:43 +0100

    Main branch - Nirali

---
+++ src/main/java/group/git/TestClass.java
@@ -0,0 +1,9 @@
+package group.git;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("by Nirali");
+        System.out.println("by riya");
+    }
+}
